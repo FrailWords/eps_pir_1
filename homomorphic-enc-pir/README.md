9d75ac7ac72d47ff26442cd970faeb699bee199f
Sriram Viswanathan <sriram.vish@gmail.com>
2023-02-10 23:02:46 -0500

    Working FHE example

--- homomorphic-enc-pir/README.md
+++ homomorphic-enc-pir/README.md
@@ -1,19 +1,6 @@
 # Requirements
 
-1. install SEAL - 
+Install tenSEAL - https://github.com/OpenMined/TenSEAL - 
 ```shell
- brew install seal
-```
-
-2. Set GCC and GXX env variables - 
-
-```shell
-export gcc=/usr/local/Cellar/gcc/12.2.0/bin/gcc-12
-export gxx=/usr/local/Cellar/gcc/12.2.0/bin/g++-12
-```
-
-4. Install Pyfhel using the following command for the latest version (v3.4.0) - 
-
-```shell
-python -m pip install Pyfhel
+ pip install tenseal
 ```
--- homomorphic-enc-pir/pir.py
+++ homomorphic-enc-pir/pir.py
@@ -1,26 +1,18 @@
+import tenseal as ts
 import numpy as np
-from Pyfhel import Pyfhel, PyPtxt, PyPtxt
 
 
 class Server:
 
     def __init__(self, size):
         print("Initializing server data:")
-
         self.size = size
-        arr = []
-        for i in range(size):
-            row = []
-            for j in range(size):
-                row.append(i + j)
-            arr.append(row)
-
-            print(row)
-        self.data_arr = np.array(arr)
+        self.data_arr = np.random.randint(1, 20, size=(size, size))
+        print("Data : ", self.data_arr)
 
     def get_data(self, enc_row, enc_col):
-        row = np.matmul(enc_row, self.data_arr)
-        return np.matmul(row, enc_col)
+        row = enc_row.matmul(self.data_arr)
+        return enc_col.dot(row)
 
     def get_data_row(self, enc_row):
         row = np.matmul(enc_row, self.data_arr)
@@ -30,45 +22,43 @@ class Server:
 class Client:
 
     def __init__(self):
-        HE = Pyfhel()
-        HE.contextGen(p=63)  # Generating context.
-        HE.keyGen()
-
-        enc = lambda x: HE.encryptInt(x)
-        self.enc_vec = np.vectorize(enc)
-
-        dec = lambda x: HE.decryptInt(x)
-        self.dec_vec = np.vectorize(dec)
+        # Setup TenSEAL context
+        self.context = ts.context(
+            ts.SCHEME_TYPE.CKKS,
+            poly_modulus_degree=8192,
+            coeff_mod_bit_sizes=[60, 40, 40, 60])
+        self.context.generate_galois_keys()
+        self.context.global_scale = 2 ** 40
 
     def req_row(self, i, size):
-        req = [0 if j != i else 1 for j in range(size)]
+        req = np.array([0 if j != i else 1 for j in range(size)])
         print("generated row req to enc:", req)
-        return self.enc_vec(np.array(req))
+        return ts.ckks_vector(self.context, req)
 
     def req_col(self, i, size):
-        req = [[0] if j != i else [1] for j in range(size)]
+        req = np.array([0 if j != i else 1 for j in range(size)])
         print("generated column req to enc:", req)
-        return self.enc_vec(np.array(req))
+        return ts.ckks_vector(self.context, req)
 
     def make_and_dec_req(self, server, row, col):
         out = server.get_data(self.req_row(row, server.size), self.req_col(col, server.size))
-        return self.dec_vec(out)
+        return out.decrypt()
 
     def make_and_dec_row_req(self, server, row):
         out = server.get_data_row(self.req_row(row, server.size))
-        return self.dec_vec(out)
+        return out.decrypt()
 
 
 def main():
     size = 10
-    row = 0
-    col = 0
+    row = 8
+    col = 9
 
     server = Server(size)
     client = Client()
 
     print("starting request ... ")
-    print("output:", client.make_and_dec_req(server, row, col))
+    print("output:", round(client.make_and_dec_req(server, row, col)[0]))
 
 
 if __name__ == "__main__":
